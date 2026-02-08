e0aac21051d8ce6cc570b932dfcae18a80ec553f
ziagulgholami641-2026 <ziagulgholami641@gmail.com>
2026-02-08 21:38:50 +0430

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+let i=28;
+let u=34;
+console.log (i*u);
+
+
+let x=24;
+let y=50;
+let z=2
+console.log(x%y+z);
+
+
+
+let a ="apple,orange,banana";
+console.log(a="this is frouts");
+
+const name="gull";
+const lastname="gholami";
+console.log(name+lastname);
+
+const massege="hi deare";
+const messege1="Good morning";
+console.log(massege+messege1);
+
+
+
